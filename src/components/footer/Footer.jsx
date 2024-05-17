371ead307d4e346c295ddd64ad2e953a5d90423e
piaan <VianRiztanta71@gmail.com>
2024-05-18 04:12:06 +0700

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>Copyright By <a href="">Andika Riztanta Previan</a></footer>
+  )
+}
+
+export default Footer
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Project</li>
+            <li>Skill</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
