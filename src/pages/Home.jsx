ca7b566d2a0eab272f68b4d5daa14b4ebf4678d9
piaan <VianRiztanta71@gmail.com>
2024-05-19 19:36:18 +0700

    TypeWriter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
   return (
     <div className="container mx-auto h-auto text-white font-sometype-mono">
       <main className="lg:w-1/2">
         <p className="font-bold text-3xl">Hello, I’m Andika.</p>
-        <div className="text-typing font-medium text-2xl mt-1">
-          <span class="">FrontEnd Development</span>
+        <div className="text-typing font-medium text-2xl mt-1 select-none">
+          <Typewriter
+            options={{
+              strings: ["FrontEnd Developer", "BackEnd Developer"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
         </div>
-        <p className="text-xl text-justify mt-2">
+        <p className="text-lg text-justify mt-2">
           I am a passionate and creative web developer with 4 years of
           experience building engaging and functional websites. For more
           information, please scroll down to see my profile.
