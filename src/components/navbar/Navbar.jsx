581ca59dd0464a0fec4903f3e5b6405f8b0c6a50
piaan <VianRiztanta71@gmail.com>
2024-05-20 20:01:38 +0700

    (#4)

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,35 @@
 import React, {useState} from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
   const [ isOpen, setIsOpen ] = useState(false);
-  
+  const location = useLocation();
+
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   }
 
-  return (
-      <nav className='p-6 font-sometype-mono'>
-        <div className="container mx-auto flex lg:flex-row flex-col justify-center items-center">
-          <div className="menu-button flex justify-end w-full lg:hidden">
-            <button onClick={toggleMenu} className="text-white focus:outline-none">
-            <svg className='fill-white' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M24,3c0,.55-.45,1-1,1H1c-.55,0-1-.45-1-1s.45-1,1-1H23c.55,0,1,.45,1,1ZM7,20H1c-.55,0-1,.45-1,1s.45,1,1,1H7c.55,0,1-.45,1-1s-.45-1-1-1ZM15,11H1c-.55,0-1,.45-1,1s.45,1,1,1H15c.55,0,1-.45,1-1s-.45-1-1-1Z"/></svg>
-            </button>
-          </div>
-          <div className={`lg:flex lg:items-center w-full lg:w-auto ${isOpen ? 'block' : 'hidden'}`}>
-            <ul className='lg:flex lg:gap-4 text-white font-normal uppercase tracking-widest'>
-              <li><Link to="/">Home</Link></li> <span className='hidden lg:block select-none'>|</span>
-              <li><Link to="/project">Project</Link></li> <span className='hidden lg:block select-none'>|</span>
-              <li><Link to="/contact">Contact</Link></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+  const getLinkClass = (path) => {
+    return location.pathname === path ? 'text-blue-500' : '';
+  }
+
+return (
+  <nav className='p-6 font-sometype-mono'>
+    <div className="container mx-auto flex lg:flex-row flex-col justify-center items-center">
+      <div className="menu-button flex justify-end w-full lg:hidden">
+        <button onClick={toggleMenu} className="text-white focus:outline-none">
+        <svg className='fill-white' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M24,3c0,.55-.45,1-1,1H1c-.55,0-1-.45-1-1s.45-1,1-1H23c.55,0,1,.45,1,1ZM7,20H1c-.55,0-1,.45-1,1s.45,1,1,1H7c.55,0,1-.45,1-1s-.45-1-1-1ZM15,11H1c-.55,0-1,.45-1,1s.45,1,1,1H15c.55,0,1-.45,1-1s-.45-1-1-1Z"/></svg>
+        </button>
+      </div>
+      <div className={`lg:flex lg:items-center w-full lg:w-auto ${isOpen ? 'block' : 'hidden'}`}>
+        <ul className='lg:flex lg:gap-4 text-white font-normal uppercase tracking-widest'>
+          <li><Link to="/" className={`hover:text-blue-500 ${getLinkClass('/')}`}>Home</Link></li> <span className='hidden lg:block select-none'>|</span>
+          <li><Link to="/project" className={`hover:text-blue-500 ${getLinkClass('/project')}`}>Project</Link></li> <span className='hidden lg:block select-none'>|</span>
+          <li><Link to="/contact" className={`hover:text-blue-500 ${getLinkClass('/contact')}`}>Contact</Link></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   )
 }
 
