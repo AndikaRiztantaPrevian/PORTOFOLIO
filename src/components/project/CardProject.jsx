18d88c3c4b02f2c937a64b9b51c244a0a24c2a14
piaan <VianRiztanta71@gmail.com>
2024-05-24 17:31:15 +0700

    Ketinggalan

--- src/components/project/CardProject.jsx
+++ src/components/project/CardProject.jsx
@@ -8,11 +8,11 @@ const CardProject = () => {
     <div className="flex justify-center items-start gap-4 w-full h-2/4" data-aos="fade-out" data-aos-duration="2000" data-aos-offset="200">
         {/* Card 1 */}
         <div class="border border-blue-300 shadow rounded-md p-4 w-80 h-auto">
-            <div class="py-1 h-full flex flex-col justify-between space-y-3">
-                <div class="text-center text-xl tracking-wider">Pomodoro</div>
+            <div class="py-1 h-full flex flex-col justify-between space-y-4">
+                <div class="text-xl tracking-wider underline underline-offset-8 select-none">Pomodoro</div>
                 <div className="text-justify text-xs sm:text-sm">This application was created to manage work and rest hours, users can set them according to their wishes.</div>
                 <div class="flex h-10">
-                    <div className="flex items-end w-2/4">
+                    <div className="flex items-end w-2/4 cursor-pointer">
                         <TechStack icon={IoLogoElectron} name="Electron" color="text-cyan-600"/>
                         <TechStack icon={IoLogoJavascript} name="Javascript" color="text-yellow-500"/>
                         <TechStack icon={DiNodejs} name="NodeJS" color="text-green-500"/>
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -9,7 +9,7 @@ const Project = () => {
         <div className="w-full h-full flex flex-col justify-center items-center mb-10 sm:mb-0 space-y-8">
           {/* Title Content */}
           <h4 className={`mb-4 sm:mb-0 ${title}`} data-aos="fade-out" data-aos-duration="1500" data-aos-offset="200">Project</h4>
-          <p className="w-full md:w-2/3 text-center text-sm sm:text-lg text-sm select-none" data-aos="fade-out" data-aos-duration="1800" data-aos-offset="200">Featured here are various projects I have undertaken, some for personal development and others commissioned by clients.</p>
+          <p className="w-full md:w-2/3 text-center text-sm sm:text-lg select-none" data-aos="fade-out" data-aos-duration="1800" data-aos-offset="200">Featured here are various projects I have undertaken, some for personal development and others commissioned by clients.</p>
           {/* Card Content */}
           <CardProject />
         </div>
