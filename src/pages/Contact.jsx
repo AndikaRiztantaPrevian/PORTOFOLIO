051f9663c833c0a6815916cef5fb885eeb569a38
piaan <VianRiztanta71@gmail.com>
2024-05-26 21:55:21 +0700

    SEO

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import FormContact from "../components/form-contact/FormContact";
 import { title } from "../styles/styles";
+import { Helmet } from "react-helmet-async";
 
 const Contact = () => {
-  return (
-    <div className="h-[90vh] lg:h-[80vh]">
-      <div className="container mx-auto w-full h-full flex flex-col justify-evenly items-center mb-5 sm:mb-0">
-        <div className="w-full h-full flex flex-col justify-end md:justify-center items-center mb-10 sm:mb-0">
-          {/* Title Content */}
-          <div className="flex flex-col justify-center items-center space-y-5">
-            <h4 className={`mb-4 sm:mb-0 ${title}`} data-aos="fade-out" data-aos-duration="1500" data-aos-offset="200">Contact</h4>
-            <p className="w-full md:w-2/3 text-center text-sm sm:text-lg select-none" data-aos="fade-out" data-aos-duration="1800" data-aos-offset="200">Should you be interested in my capabilities, please feel free to contact me.</p>
-          </div>
-          {/* Form */}
-          <FormContact />
+  return <>
+  <div className="h-[90vh] lg:h-[80vh]">
+    <Helmet>
+      <title>Contact - Andika Riztanta Previan</title>
+      <meta name="description" content="Should you be interested in my capabilities, please feel free to contact me." />
+      <link rel="canonical" href="/contact" />
+    </Helmet>
+    <div className="container mx-auto w-full h-full flex flex-col justify-evenly items-center mb-5 sm:mb-0">
+      <div className="w-full h-full flex flex-col justify-end md:justify-center items-center mb-10 sm:mb-0">
+        {/* Title Content */}
+        <div className="flex flex-col justify-center items-center space-y-5">
+          <h4 className={`mb-4 sm:mb-0 ${title}`} data-aos="fade-out" data-aos-duration="1500" data-aos-offset="200">Contact</h4>
+          <p className="w-full md:w-2/3 text-center text-sm sm:text-lg select-none" data-aos="fade-out" data-aos-duration="1800" data-aos-offset="200">Should you be interested in my capabilities, please feel free to contact me.</p>
         </div>
+        {/* Form */}
+        <FormContact />
       </div>
     </div>
-  );
+  </div>
+  </>;
 };
 
 export default Contact;
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import CardProject from '../components/project/CardProject'
 import { title } from "../styles/styles";
+import { Helmet } from 'react-helmet-async';
 
 const Project = () => {
-  return (
-    <div className='h-[90vh] lg:h-[80vh]'>
-      <div className="container mx-auto w-full h-full flex flex-col justify-evenly items-center mb-5 sm:mb-0">
-        <div className="w-full h-full flex flex-col justify-end lg:justify-center items-center mb-10 sm:mb-0 space-y-8">
-          {/* Title Content */}
-          <h4 className={`mb-4 sm:mb-0 ${title}`} data-aos="fade-out" data-aos-duration="1500" data-aos-offset="200">Project</h4>
-          <p className="w-full md:w-2/3 text-center text-sm sm:text-lg select-none" data-aos="fade-out" data-aos-duration="1800" data-aos-offset="200">Featured here are various projects I have undertaken, some for personal development and others commissioned by clients.</p>
-          {/* Card Content */}
-          <CardProject />
-        </div>
+  return <>
+  <Helmet>
+    <title>Project - Andika Riztanta Previan</title>
+    <meta name="description" content="Featured here are various projects I have undertaken, some for personal development and others commissioned by clients." />
+    <link rel="canonical" href="/project" />
+  </Helmet>
+  <div className='h-[90vh] lg:h-[80vh]'>
+    <div className="container mx-auto w-full h-full flex flex-col justify-evenly items-center mb-5 sm:mb-0">
+      <div className="w-full h-full flex flex-col justify-end lg:justify-center items-center mb-10 sm:mb-0 space-y-8">
+        {/* Title Content */}
+        <h4 className={`mb-4 sm:mb-0 ${title}`} data-aos="fade-out" data-aos-duration="1500" data-aos-offset="200">Project</h4>
+        <p className="w-full md:w-2/3 text-center text-sm sm:text-lg select-none" data-aos="fade-out" data-aos-duration="1800" data-aos-offset="200">Featured here are various projects I have undertaken, some for personal development and others commissioned by clients.</p>
+        {/* Card Content */}
+        <CardProject />
       </div>
     </div>
-  )
+  </div>
+  </>
 }
 
 export default Project
