d0be1933b7d8170fbe2af85da40a58f729763ad4
piaan <VianRiztanta71@gmail.com>
2024-05-27 06:35:25 +0700

    responsive

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,8 +11,8 @@ const Contact = () => {
       <meta name="description" content="Should you be interested in my capabilities, please feel free to contact me." />
       <link rel="canonical" href="/contact" />
     </Helmet>
-    <div className="container mx-auto w-full h-full flex flex-col justify-evenly items-center mb-5 sm:mb-0">
-      <div className="w-full h-full flex flex-col justify-end md:justify-center items-center mb-10 sm:mb-0">
+    <div className="container mx-auto w-full h-full mb-5 sm:mb-0">
+      <div className="w-full h-full flex flex-col justify-center md:justify-center items-center mb-10 sm:mb-0">
         {/* Title Content */}
         <div className="flex flex-col justify-center items-center space-y-5">
           <h4 className={`mb-4 sm:mb-0 ${title}`} data-aos="fade-out" data-aos-duration="1500" data-aos-offset="200">Contact</h4>
