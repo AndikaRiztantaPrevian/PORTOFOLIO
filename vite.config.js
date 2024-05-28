654d35c0d829c9555dcc8cdd691ec6476d7670c7
piaan <VianRiztanta71@gmail.com>
2024-05-28 08:27:48 +0700

    configure server

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react(), viteCompression({ algorithm: "gzip" })],
 });
